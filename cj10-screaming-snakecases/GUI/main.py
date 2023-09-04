951e4825ea8b2a6130c80d0a3562cc50f78b356d
Techlord210 <techlord210@gmail.com>
2023-09-05 00:42:37 +0530

    Added Configuration Bar and using StyleSheets for toolbars

--- cj10-screaming-snakecases/GUI/main.py
+++ cj10-screaming-snakecases/GUI/main.py
@@ -16,33 +16,61 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setWindowTitle('cj10-screaming-snakecases')
 
-        self.top_bar()
-        self.left_bar()
+        self.setStyleSheet('background: #efefef;')
 
-    def left_bar(self) -> None:
+        self.addToolBar(QtCore.Qt.ToolBarArea.LeftToolBarArea, self.left_bar())
+        self.addToolBar(QtCore.Qt.ToolBarArea.TopToolBarArea, self.top_bar())
+        self.addToolBar(QtCore.Qt.ToolBarArea.RightToolBarArea, self.right_bar())
+
+    def left_bar(self) -> QtWidgets.QToolBar:
         tool_bar = QtWidgets.QToolBar('Tool Bar')
         tool_bar.setMovable(False)
         tool_bar.setFixedWidth(40)
+        tool_bar.setStyleSheet(
+            'background: #ffffff;'
+            'border-right: 0.5px solid #d8d8d8;'
+        )
 
-        self.addToolBar(QtCore.Qt.ToolBarArea.LeftToolBarArea, tool_bar)
+        return tool_bar
 
-    def top_bar(self) -> None:
+    def top_bar(self) -> QtWidgets.QToolBar:
         tool_bar = QtWidgets.QToolBar('placeholder')
         tool_bar.setMovable(False)
         tool_bar.setFixedHeight(35)
+        tool_bar.setStyleSheet(
+            'background: #ffffff;'
+            'border-bottom: 0.5px solid #e0e0e0;'
+        )
 
         self.title = QtWidgets.QLabel('placeholder')
         self.title.setStyleSheet(
-            'padding-left: 10px;'
             'font-family: monospace;'
             'font-size: 15px;'
             'font-weight: 550;'
             'letter-spacing: 1.2px;'
         )
 
-        tool_bar.addWidget(self.title)
+        left_spacer = QtWidgets.QWidget()
+        left_spacer.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Preferred)
+        right_spacer = QtWidgets.QWidget()
+        right_spacer.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Preferred)
 
-        self.addToolBar(QtCore.Qt.ToolBarArea.TopToolBarArea, tool_bar)
+        tool_bar.addWidget(left_spacer)
+        tool_bar.addWidget(self.title)
+        tool_bar.addWidget(right_spacer)
+
+        return tool_bar
+
+    def right_bar(self) -> QtWidgets.QToolBar:
+        tool_bar = QtWidgets.QToolBar('Configuration Bar')
+        tool_bar.setMovable(False)
+        tool_bar.setFixedWidth(175)
+        tool_bar.setStyleSheet(
+            'background: #ffffff;'
+            'border-left: 0.5px solid #d8d8d8;'
+        )
+
+        return tool_bar
 
 def main() -> None:
     app = QtWidgets.QApplication([])
