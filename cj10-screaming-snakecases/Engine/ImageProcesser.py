cdc3b7c4e239304097c75f27f0c9ebbc47c97174
AstroKhet <liugaoyuan123@gmail.com>
2023-09-06 04:58:41 +1000

    Basic Image Tools and scaffolding for Image processing and viewing
    I am not so sure if this structure is optimal for the project. tbh i'm also not too sure what im doing lol. any errors just ping in discord

---
+++ cj10-screaming-snakecases/Engine/ImageProcesser.py
@@ -0,0 +1,21 @@
+# This file should contain image processing functions, as ImageTools contain image manipulation functions
+# Potential things to add (@Envsxion)
+
+# Forgery detection: Implement algorithms to detect image tampering or forgery.
+# Steganography tools: Kinda self explanatory.
+# Hashing and authentication: Implement image hashing and authentication mechanisms.
+# Noise Analysis
+# Blur Detection
+# Content-Based Image Retrieval (CBIR): dunno how but ig we can just use an api or send it off to google images
+# OCR (Optical Character Recognition)
+# Geolocation analysis 
+# Image Comparison (finding mathematical differences between images to reveal the flag??)
+# File Format Analysis (images inside an image are a great way to f ppl over) 
+
+
+# Seems like quite a bit though, maybe we could come up with a more concrete direction for our game, and narrow down the scope?
+# Brings in opportunities to differntiate the final product also; seems like many would come up with the same idea
+
+class ImageProcessor:
+    def __init__(self) -> None:
+        pass
