d9e26d8da5beeefd50f9ad100d73b5a02b639f0e
Envxsion <74938513+Envxsion@users.noreply.github.com>
2023-09-07 07:48:06 +1000

    Fix Overwrite

--- cj10-screaming-snakecases/Engine/test/tool/metadata.py
+++ cj10-screaming-snakecases/Engine/test/tool/metadata.py
@@ -6,7 +6,7 @@ logging.basicConfig(filename='cj10-screaming-snakecases/Engine/test/logs/image_m
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load the image
-image_path = 'cj10-screaming-snakecases/Engine/test/img/flag_geoloc_image.png'  # Update with your image path
+image_path = 'cj10-screaming-snakecases/Engine/test/img/flag_metadata_image.png'  # Update with your image path
 
 try:
     image = Image.open(image_path)
