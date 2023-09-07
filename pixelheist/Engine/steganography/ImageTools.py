248cdcd1b94ee5ad0d70d49cf52bafa4462e5fae
AstroKhet <liugaoyuan123@gmail.com>
2023-09-08 01:10:20 +1000

    Revert "Additional functions to Image manipulation tools"
    This reverts commit 5e2c4b383186d66c97a502d13341d729b9fa0df2.

--- pixelheist/Engine/steganography/ImageTools.py
+++ pixelheist/Engine/steganography/ImageTools.py
@@ -6,57 +6,60 @@
 
 
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter
 
 # I have included some basic image manipulation tools here
 
 
 class ImageTools:
-    def __init__(self, image: Image) -> None:
+    def __init__(self, image, toArray=True) -> None:
+        if toArray:
+            image = np.asarray(image)
+
         self.image = image
-        self.imArray = np.asarray(image)
-        self.ROI = self.imArray.copy()
+        self.ROI = image.copy()
 
     def set_ROI(self, x, y, w, h):
         # Sets a Region Of Interest (ROI). This is useful for when we only want to zoom in on
         # a portion of the image for displaying, etc. By default, ROI = entire image
 
-        self.ROI = self.imArray[y: y + h, x: x + w]
+        self.ROI = self.image[y: y + h, x: x + w]
 
-    def zoom(self, scale: float) -> Image:
-        h, w, _ = self.imArray.shape
+    def zoom(self, scale, x, y, w, h):
         new_w = int(w * scale)
         new_h = int(h * scale)
 
-        resized = Image.fromarray(self.ROI.resize((new_w, new_h)))
+        resized = np.array(Image.fromarray(self.ROI).resize((new_w, new_h)))
 
         return resized
 
-    def contrast(self, factor: float) -> Image:
+    def contrast(self, factor):
         # 0 < factor < 1: Decrease contrast
         # factor = 1: No change
         # factor > 1: Increase contrast
 
-        temp = self.ROI
+        temp = Image.fromarray(self.ROI)
 
         enhancer = ImageEnhance.Contrast(temp)
         contrasted_image = enhancer.enhance(factor)
 
-        return contrasted_image
+        contrasted_imArray = np.asarray(contrasted_image)
+        return contrasted_imArray
 
-    def brightness(self, brightness: float) -> Image:
+    def brightness(self, brightness):
         # 0 < brightness < 1: Decrease contrast
         # brightness = 1: No change
         # brightness > 1: Increase brightness
 
-        temp = self.ROI
+        temp = Image.fromarray(self.ROI)
 
         enhancer = ImageEnhance.Brightness(temp)
         brightened_image = enhancer.enhance(brightness)
 
-        return brightened_image
+        brightened_imArray = np.asarray(brightened_image)
+        return brightened_imArray
 
-    def sharpness(self, sharpness: float) -> Image:
+    def sharpness(self, sharpness):
         # 0 < sharpness < 1: Decrease sharpness
         # sharpness = 1: No change
         # sharpness > 1: Increase sharpness
@@ -66,18 +69,16 @@ class ImageTools:
         enhancer = ImageEnhance.Sharpness(temp)
         sharpened_image = enhancer.enhance(sharpness)
 
-        return sharpened_image
+        sharpened_imArray = np.asarray(sharpened_image)
+        return sharpened_imArray
 
-    def color_channel(self, channel: str) -> Image:
-        # channel = RGB(A)
-        colorID = 'RGBA'.index(channel)
-        
-        h, w, channels = self.image.shape
+    def gaussian_blur(self, radius):
+        temp = Image.fromarray(self.ROI)
 
-        filtered_image = np.zeros((h, w, channels))
-        filtered_image[:, :, colorID] = self.imArray[:, :, colorID]
+        blurred_image = temp.filter(ImageFilter.GaussianBlur(radius))
+        blurred_imArray = np.asarray(blurred_image)
 
-        return Image.fromarray(filtered_image)
+        return blurred_imArray
 
 
 if __name__ == "__main__":
