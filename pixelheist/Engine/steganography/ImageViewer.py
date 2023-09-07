5d951dacd614279487b797c42be1f22451404639
AstroKhet <liugaoyuan123@gmail.com>
2023-09-08 01:18:25 +1000

    Revert "Revert "Additional functions to Image manipulation tools""
    This reverts commit 248cdcd1b94ee5ad0d70d49cf52bafa4462e5fae.

--- pixelheist/Engine/steganography/ImageViewer.py
+++ pixelheist/Engine/steganography/ImageViewer.py
@@ -4,6 +4,29 @@
 # This will have to be programmed in conjunction with the GUI soon.
 
 
+from PIL import Image
+import numpy as np
+
+
 class ImageViewer():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, image: Image) -> None:
+        self.image = image
+        self.imArray = np.asarray(image)
+
+    def histogram(self):
+        intensities = np.zeros((4, 256))  # 4 color channel - RGB(A), and 256 possible intensity values
+        h, w, _ = self.imArray.shape
+
+        channels = self.image.split()
+
+        for channel, channel_imArray in enumerate(channels):
+            intensities[channel] = channel_imArray.histogram()
+
+        return intensities
+
+
+if __name__ == '__main__':
+    img = Image.open(r"pixelheist\Engine\test\img\testimage.png")
+    temp = ImageViewer(img)
+
+    print(temp.histogram()[0])  # Red colour
