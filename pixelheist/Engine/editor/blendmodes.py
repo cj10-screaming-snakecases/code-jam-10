de35673ab16d86c6aee6b5f8e5c243cd1ae3fe9a
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-09-10 14:21:09 +0800

    Update blendmodes to use numpy arrays

--- pixelheist/Engine/editor/blendmodes.py
+++ pixelheist/Engine/editor/blendmodes.py
@@ -1,20 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
 
 import numpy as np
 from PIL import Image
 
 # Blend modes take 2 images (the base and the new image), and returns a new image
 # The base image may be modified in the process
-BlendMode = Callable[[Image.Image, Image.Image], Image.Image]
+ImageArray = np.ndarray[Any, np.uint8]
+
+BlendMode = Callable[[ImageArray, ImageArray], ImageArray]
 
 
-def none(base: Image.Image, new: Image.Image) -> Image.Image:
+def none(base: ImageArray, new: ImageArray) -> ImageArray:
+    base = Image.fromarray(base)
+    new = Image.fromarray(new)
     base.paste(new)
-    return base
+    return np.asarray(base)  # type: ignore
 
 
-def multiply(base: Image.Image, new: Image.Image) -> Image.Image:
-    base = np.asarray(base)
-    new = np.asarray(new)
-    result = base * new // 255
-    return Image.fromarray(result)
+def multiply(base: ImageArray, new: ImageArray) -> ImageArray:
+    return base * new // 255
