0b8df80ccce750ca395fde8cbc13264f35047f7b
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-09-10 16:21:05 +0800

    Fix lint

--- pixelheist/Engine/editor/__init__.py
+++ pixelheist/Engine/editor/__init__.py
@@ -1,2 +1,2 @@
-from pixelheist.Engine.editor import blendmodes
-from pixelheist.Engine.editor.editor import Editor
+from pixelheist.Engine.editor import blendmodes  # noqa: F401
+from pixelheist.Engine.editor.editor import Editor  # noqa: F401
